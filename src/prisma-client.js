4d94d742200c29beda92d662ffe1c0e8472eb179
Ricky Andrianto K <ricky@codr.id>
2025-12-24 18:44:21 +0700

    Initial Setup

---
+++ src/prisma-client.js
@@ -0,0 +1,10 @@
+import {PrismaClient} from '@prisma/client';
+
+export const prismaClient = new PrismaClient(
+    {
+      errorFormat:'pretty',
+      log:[
+          "query",'info','warn','error'
+      ]
+    }
+)
